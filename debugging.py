eb12c04adfa1ce1bec67ea35908c9ce8222f07fa
aaxlss <axl9305@gmail.com>
2022-09-01 13:19:58 +0100

    [Update] Updating functionality to validate invalid characters and numbers, using try, except and raise

--- debugging.py
+++ debugging.py
@@ -2,8 +2,16 @@ def division(num):
     return [number for number in range(1, num + 1) if num % number == 0]
 
 def run():
-    num = int(input('Insert a number: '))
-    print(division(num))
-
+    try:
+        num = int(input('Insert a number: '))
+        if num <= 0:
+            raise Exception('Number not allow')
+        print(division(num))
+    except ValueError as v:
+        print(v)
+        print('Numbers are just allow')
+    except Exception as e:
+        print(e)
+        print('Number must be greater that 0')
 if __name__ == '__main__':
     run()
